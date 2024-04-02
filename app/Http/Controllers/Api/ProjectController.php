c18254f58dfa73e20175b96a5a3ced57c0b5e412
emilianogastaldo <emiliano0303@gmail.com>
2024-04-02 17:24:22 +0200

    creata l'api show con l'id

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -33,9 +33,11 @@ class ProjectController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Project $project)
+    public function show(string $id)
     {
-        //
+        $project = Project::whereIsPublished(true)->find($id); //->whereSlug($slug);
+        if (!$project) return response(null, 404);
+        return response()->json($project);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -21,12 +21,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // Rotta per recuperare i progetti dal database con json 
 // (ricordati che aggiunge in automatico api davanti a /projects)
-Route::get('/projects', [ProjectController::class, 'index']);
+// Route::get('/projects', [ProjectController::class, 'index']);
 // Le altre rotte che mi serviranno in futuro sono:
 // Route::get('/projects/{project}', [ProjectController::class, 'show']);
 // Route::post('/projects/{project}', [ProjectController::class, 'store']);
 // Route::delete('/projects/{project}', [ProjectController::class, 'destroy']);
 // Route::put('/projects/{project}', [ProjectController::class, 'upadate']);
 
-// Posso scrivere tutte le rotte manualmente oppure con:
-// Route::apiResource('/projects', ProjectController::class);
+// Posso scrivere tutte le rotte manualmente oppure con: only mi permette di dirgli quali usare
+Route::apiResource('/projects', ProjectController::class)->only('index');
+Route::get('/projects/{id}', [ProjectController::class, 'show']);
+
+// Comando per vedere le rotte: php artisan route:list e posso filtrare con --path=api
