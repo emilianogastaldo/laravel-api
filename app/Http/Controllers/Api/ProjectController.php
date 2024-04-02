937199006ae59116ce9ecfe0262bb5536c20ac41
emilianogastaldo <emiliano0303@gmail.com>
2024-04-02 16:16:05 +0200

    aggiunta paginazione

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -13,7 +13,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::whereIsPublished(true)->with('type', 'technologies')->latest()->get();
+        $projects = Project::whereIsPublished(true)->with('type', 'technologies')->latest()->paginate(3);
         // Qui avrei un problema con i link delle immagini, che per ora sarebbero realtivi, invece sarebbe meglio aver assoluti
         // O faccio un ciclo ora, oppure vado a creare un Accessor nel modello Project che modifica in automatico l'url.
         foreach ($projects as $project) {
