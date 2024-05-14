32851e9ee64739354c9479d84fed1863f12685bd
emilianogastaldo <emiliano0303@gmail.com>
2024-05-14 18:07:27 +0200

    sistemata l'api per i type

--- app/Http/Controllers/Api/TypeProjectController.php
+++ app/Http/Controllers/Api/TypeProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use App\Models\Type;
 use Illuminate\Http\Request;
 
@@ -15,8 +16,12 @@ class TypeProjectController extends Controller
         // Se invio giù questi progetti sono senza la relazione con le tecnologie, 
         // $projects = $type->projects;
         // Per inserire la relazione devo usare PRIMA il metodo load()
-        $type->load('projects.user', 'projects.technology', 'projects.type');
-        $projects = $type->projects->where('is_published', 1);
+        // $type->load('projects.user', 'projects.technologies', 'projects.type');
+        // Con questo where mi da un oggetto e non un array, quindi bisogna cambiare approccio
+        // $projects = $type->projects->where('is_published', 1);
+
+        // Riscrivo per avere un array come risultato
+        $projects = Project::whereTypeId($type->id)->whereIsPublished(true)->with('user', 'technologies', 'type')->get();
 
         return response()->json(['projects' => $projects, 'label' => $type->label]);
     }
